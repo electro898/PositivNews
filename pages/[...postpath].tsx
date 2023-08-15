d4b66375dc27ba64fdc7b724cf5e047cfe9c6451
electro898 <101351317+electro898@users.noreply.github.com>
2023-08-15 17:04:55 +0200

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://dailynews24.info/graphql"
+	const endpoint = "https://thejournal24h.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://dailynews24.info/` + encodeURI(path as string)
+					`https://thejournal24h.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
