19803cb02c034a3cae9286708638b11e7fc7ffef
electro898 <101351317+electro898@users.noreply.github.com>
2023-08-15 17:13:28 +0200

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,13 +4,26 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://thejournal24h.com/graphql"
+	const endpoint = "https://alcashzone.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
 	const path = pathArr.join('/');
 	console.log(path);
 	const fbclid = ctx.query.fbclid;
+
+	// redirect if facebook is the referer or request contains fbclid
+		if (referringURL?.includes('facebook.com') || fbclid) {
+
+		return {
+			redirect: {
+				permanent: false,
+				destination: `${
+					`https://alcashzone.com/` + encodeURI(path as string)
+				}`,
+			},
+		};
+		}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
